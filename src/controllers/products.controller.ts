6b393c27451f0af9afa3a62107870e7d8441d464
Lucas Torres <lucastorreslr10@gmail.com>
2025-06-17 20:17:30 -0300

    feat: add POST product and GET product with query filter

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,9 +1,34 @@
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex.js";
+import { z } from "zod/v4";
 
 export class ProductsController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
-      return res.json({ message: "ok" });
+      const { name } = req.query;
+
+      const products = await knex<ProductRepository>("products")
+        .select()
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name");
+
+      return res.json(products);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(3),
+        price: z.number().gt(0),
+      });
+      const { name, price } = bodySchema.parse(req.body);
+
+      await knex<ProductRepository>("products").insert({ name, price });
+
+      return res.status(201).json();
     } catch (err) {
       next(err);
     }
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod/v4";
 import { AppError } from "@/utils/AppError.js";
 
 export function errorHandler(
@@ -9,6 +10,12 @@ export function errorHandler(
 ) {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .json({ message: "validation error", issues: error.format });
   }
 
   return res.status(500).json({ message: error.message });
