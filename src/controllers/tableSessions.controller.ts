02643730f1f960c5c6c1c65c66b086acece6f294
Lucas Torres <lucastorreslr10@gmail.com>
2025-06-17 22:44:32 -0300

    feat: add PATCH to close a session

--- src/controllers/tableSessions.controller.ts
+++ src/controllers/tableSessions.controller.ts
@@ -44,4 +44,37 @@ export class TablesSessionsController {
       next(err);
     }
   }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { error: "id must be a number" })
+        .parse(req.params.id);
+
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .select()
+        .where({ id })
+        .first();
+
+      if (!session) {
+        throw new AppError("session table not found");
+      }
+
+      if (session.closed_at) {
+        throw new AppError("this session is already closed");
+      }
+
+      await knex<TablesSessionsRepository>("tables_sessions")
+        .update({
+          closed_at: knex.fn.now(),
+        })
+        .where({ id });
+
+      return res.json();
+    } catch (err) {
+      next(err);
+    }
+  }
 }
